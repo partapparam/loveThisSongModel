e2b5b86385ff85935efe1834202214ad1155d558
partapparam <paramsingh1961@gmail.com>
2023-12-20 12:46:43 -0800

    successful build in docker

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import xgboost as xgb
-from train import prediction
-
+import pandas as pd
 
 
 model_file = "xgb_model.bin"
@@ -14,6 +13,27 @@ with open(model_file, "rb") as f_in:
     dv, xgb_model = pickle.load(f_in)
 
 app = Flask(__name__)
+
+# function to predict using the model and DictVectorizer
+def prediction(data, dv, model):
+    array = [data]
+    df = pd.DataFrame(array)
+    del df['song_title']
+
+    y_test = df['target']
+    del df['target']
+    features = list(dv.get_feature_names_out())
+
+    df_dict = df.to_dict(orient='records')
+
+    X_test = dv.transform(df_dict)
+    dtest = xgb.DMatrix(X_test, label=y_test, feature_names=features)
+
+    xgb_pred = model.predict(dtest)
+    xgb_liked = (xgb_pred >= 0.5)
+    result = {"Like probability": float(xgb_pred), "Liked": bool(xgb_liked)}
+
+    return result
 
 
 @app.route("/predict", methods=["POST"])
